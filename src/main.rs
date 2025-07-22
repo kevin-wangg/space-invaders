f3807feaba02f55b8933540ae23639cb31c7e52e
Kevin Wang <kevinwang2040@gmail.com>
2025-07-21 23:52:02 -0700

    Simple example

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,77 @@
+use pixels::{Pixels, SurfaceTexture};
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowAttributes, WindowId};
+use winit::window::{Window, WindowId};
 
-const WIDTH: usize = 320;
-const HEIGHT: usize = 240;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
-#[derive(Default)]
 struct App {
-    window: Option<Window>,
+    pixels: Option<Pixels<'static>>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            pixels: None,
+        }
+    }
+
+    fn draw(&mut self) {
+        if let Some(ref mut pixels) = self.pixels {
+            let frame = pixels.frame_mut();
+
+            for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+                let x = i % WIDTH as usize;
+                let y = i / WIDTH as usize;
+
+                if x > 200 && x < 600 && y > 200 && y < 400 {
+                    pixel[0] = 255;
+                } else {
+                    pixel[0] = 0;
+                }
+                pixel[3] = 255; // Alpha
+            }
+
+            pixels.render().unwrap();
+        }
+    }
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap())
+        let window = event_loop
+            .create_window(
+                Window::default_attributes()
+                    .with_title("Space Invaders")
+                    .with_inner_size(LogicalSize::new(WIDTH, HEIGHT)),
+            )
+            .unwrap();
+
+        let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, window);
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap();
+
+        self.pixels = Some(pixels);
+        self.draw();
     }
 
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
-        window_id: WindowId,
+        _window_id: WindowId,
         event: WindowEvent,
     ) {
         match event {
             WindowEvent::CloseRequested => {
-                println!("Close button was pressed");
+                println!("Closing window!");
                 event_loop.exit();
-            },
-            WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
             }
-            _ => ()
+            WindowEvent::RedrawRequested => {
+                self.draw();
+            }
+            _ => {}
         }
     }
 }
@@ -39,11 +79,7 @@ impl ApplicationHandler for App {
 fn main() {
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
-    // let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-    // let window_attributes = WindowAttributes::default()
-    //     .with_title("Space Invader Game")
-    //     .with_inner_size(size)
-    //     .with_min_inner_size(size);
-    let mut app = App::default();
-    event_loop.run_app(&mut app);
+
+    let mut app = App::new();
+    event_loop.run_app(&mut app).unwrap();
 }
