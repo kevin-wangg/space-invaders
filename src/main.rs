b8cb6e72dd07ebd9918c204bf67aa9efdecff602
Kevin Wang <kevinwang2040@gmail.com>
2025-07-27 23:56:31 -0700

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,2 @@
-mod app;
-mod sprite;
-mod world;
-
-use crate::app::App;
-
-use winit::event_loop::EventLoop;
-
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-    let mut app = App::new().unwrap();
-    event_loop.run_app(&mut app).unwrap();
 }
