85e96b148a7bff6fbfc6b755350196ae88ed5c7a
Kevin Wang <kevinwang2040@gmail.com>
2025-07-31 00:13:31 -0400

    Use macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
-fn main() {
+use macroquad::prelude::*;
+
+#[macroquad::main("Hello world game")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        draw_text("Hello Macroquad!", 20.0, 40.0, 30.0, BLACK);
+        next_frame().await
+    }
 }
